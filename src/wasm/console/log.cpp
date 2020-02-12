f4609dd555d6152b1d5074de9335c32f92dc31b9
JarateKing <scmacintosh@upei.ca>
2020-02-12 14:41:52 -0400

    added logging for non-strings

--- src/wasm/console/log.cpp
+++ src/wasm/console/log.cpp
@@ -6,4 +6,20 @@ namespace engine
 	void ConsoleLog(std::string line) {
 		std::cout << line << '\n';
 	}
+	
+	void ConsoleLog(int value) {
+		std::cout << value << '\n';
+	}
+	
+	void ConsoleLog(long value) {
+		std::cout << value << '\n';
+	}
+	
+	void ConsoleLog(float value) {
+		std::cout << value << '\n';
+	}
+	
+	void ConsoleLog(double value) {
+		std::cout << value << '\n';
+	}
 }
--- src/wasm/console/log.h
+++ src/wasm/console/log.h
@@ -3,4 +3,8 @@
 namespace engine
 {
 	void ConsoleLog(std::string line);
+	void ConsoleLog(int value);
+	void ConsoleLog(long value);
+	void ConsoleLog(float value);
+	void ConsoleLog(double value);
 }
