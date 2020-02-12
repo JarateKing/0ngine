2b40b26a79cc54417c7c024b0cc63d9b397e6518
JarateKing <scmacintosh@upei.ca>
2020-02-12 13:28:04 -0400

    updated build script to copy resources

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 rmdir /s /q build
+
 mkdir build
 copy src\css\canvas.css build\canvas.css
+
+mkdir build\resource
+xcopy /E resource build\resource
+
 em++ -O2 src/wasm/example.cpp src/wasm/console/log.cpp --js-library src/javascript/library.js --shell-file src/html/template.html -o build/main.html -s WASM=1
