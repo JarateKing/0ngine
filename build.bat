2c479e277d640acd79b33f550ffe9b04261ddd46
JarateKing <scmacintosh@upei.ca>
2020-02-17 15:11:41 -0400

    updated build script to find all .cpp files

--- build.bat
+++ build.bat
@@ -1,9 +1,19 @@
+setlocal ENABLEDELAYEDEXPANSION
+
+:: delete previous build
 rmdir /s /q build
 
+:: copy css to build folder
 mkdir build
 copy src\css\canvas.css build\canvas.css
 
+:: copy resources to build folder
 mkdir build\resource
 xcopy /E resource build\resource
 
-em++ -O2 src/wasm/example.cpp src/wasm/console/log.cpp src/wasm/math/basic/lerp.cpp src/wasm/math/rng.cpp src/wasm/math/basic/bounds.cpp --js-library src/javascript/library.js --shell-file src/html/template.html -o build/main.html -s WASM=1
+:: find all cpp files
+@set cppFilelist=
+for /r src\wasm\ %%i in (*.cpp) do set cppFilelist=!cppFilelist! %%i
+
+:: compile cpp to wasm
+em++ -O2 %cppFilelist% --js-library src/javascript/library.js --shell-file src/html/template.html -o build/main.html -s WASM=1
