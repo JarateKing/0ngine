7a870c8615e0ad4a3d91a9c5a06abd4eb84e8fb1
JarateKing <scmacintosh@upei.ca>
2020-02-17 12:56:38 -0400

    added simple color conversions

--- src/wasm/math/types/color.h
+++ src/wasm/math/types/color.h
@@ -31,6 +31,27 @@ namespace engine
 		color(vec3 const& rgb) : r(rgb.x), g(rgb.y), b(rgb.z), a(1) {}
 		color(vec4 const& rgba) : r(rgba.x), g(rgba.y), b(rgba.z), a(rgba.w) {}
 		color(double r, double g, double b, double a) : r(r), g(g), b(b), a(a) {}
+		
+		colorRGBint toRGBint() {
+			return {r * 255 + ROUNDUP_EPSILON, g * 255 + ROUNDUP_EPSILON, b * 255 + ROUNDUP_EPSILON};
+		}
+		colorRGBAint toRGBAint() {
+			return {r * 255 + ROUNDUP_EPSILON, g * 255 + ROUNDUP_EPSILON, b * 255 + ROUNDUP_EPSILON, a * 255 + ROUNDUP_EPSILON};
+		}
+		unsigned int toARGB(bool includeAlpha = true) {
+			unsigned int alpha = (includeAlpha)?((unsigned int)(a * 255 + ROUNDUP_EPSILON) << 24) & 0xFF000000 : 0xFF000000;
+			unsigned int red = ((unsigned int)(r * 255 + ROUNDUP_EPSILON) << 16) & 0x00FF0000;
+			unsigned int green = ((unsigned int)(g * 255 + ROUNDUP_EPSILON) << 8) & 0x0000FF00;
+			unsigned int blue = (unsigned int)(b * 255 + ROUNDUP_EPSILON) & 0x000000FF;
+			return alpha | red | green | blue;
+		}
+		unsigned int toABGR(bool includeAlpha = true) {
+			unsigned int alpha = (includeAlpha)?((unsigned int)(a * 255 + ROUNDUP_EPSILON) << 24) & 0xFF000000 : 0xFF000000;
+			unsigned int blue = ((unsigned int)(b * 255 + ROUNDUP_EPSILON) << 16) & 0x00FF0000;
+			unsigned int green = ((unsigned int)(g * 255 + ROUNDUP_EPSILON) << 8) & 0x0000FF00;
+			unsigned int red = (unsigned int)(r * 255 + ROUNDUP_EPSILON) & 0x000000FF;
+			return alpha | blue | green | red;
+		}
 	};
 }
 
