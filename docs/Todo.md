c9a52d35e9cf3e44b48771e88483b4333463f0e6
JarateKing <scmacintosh@upei.ca>
2020-03-02 10:46:12 -0400

    made print out code snippets of the @TODO

--- docs/Todo.md
+++ docs/Todo.md
@@ -3,17 +3,63 @@
 ## wasm\fps\fps_counter.cpp
 
 Line 29
+```
+@TODO: make not recalc average every time
+```
+
 
 ## wasm\math\projmatrix.cpp
 
 Line 26
+```
+@TODO: implement this
+```
+
 Line 31
+```
+@TODO: implement this
+```
+
 Line 36
+```
+@TODO: implement this
+```
+
 Line 41
+```
+@TODO: implement this
+```
+
 Line 46
+```
+@TODO: implement this
+```
+
 Line 51
+```
+@TODO: implement this
+```
+
 Line 56
+```
+@TODO: implement this
+```
+
 Line 61
+```
+@TODO: implement this
+```
+
 Line 66
+```
+@TODO: implement this
+```
+
 Line 86
+```
+@TODO:
+- support near and far values from planes
+- support orthographic and partially-orthographic frustums
+```
+
 
--- docs/gen_todos.py
+++ docs/gen_todos.py
@@ -5,20 +5,27 @@ readme = open('./Todo.md', 'w+')
 readme.write('# TODOs\n\n')
 
 for (dirpath, dirnames, filenames) in walk('..\\src\\'):
-	for filename in filenames:
-		print(dirpath + '\\' + filename)
+	for (filename) in filenames:
 		file = open(dirpath + '\\' + filename)
 		line = file.readline()
 		lineNumber = 1
 		
 		todosInFile = ''
 		
-		while line != '':
+		while (line != ''):
 			search = line.find('@TODO')
 			if (search != -1):
-				todosInFile = todosInFile + '\nLine ' + str(lineNumber)
-			line = file.readline()
-			lineNumber = lineNumber + 1
+				linePrefix = line[:search]
+				todosInFile += '\nLine ' + str(lineNumber) + '\n'
+				todosInFile += '```\n'
+				while (line != '' and line.startswith(linePrefix)):
+					todosInFile += line[search:]
+					line = file.readline()
+					lineNumber += 1
+				todosInFile += '```\n'
+			else:
+				line = file.readline()
+				lineNumber += 1
 		
 		if (len(todosInFile) > 0):
 			readme.write('## ' + dirpath[7:] + '\\' + filename + '\n' + todosInFile + '\n\n')
