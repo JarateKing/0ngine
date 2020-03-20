39521105a152c5356d6a3e4aeb680a6d1df4d6bf
JarateKing <scmacintosh@upei.ca>
2020-03-20 17:41:09 -0300

    added basic quaternion math

--- src/wasm/math/types/quaternion.h
+++ src/wasm/math/types/quaternion.h
@@ -4,5 +4,32 @@ namespace engine
 		double x, y, z, w;
 		
 		quaternion() : x(0), y(0), z(0), w(1) {}
+		quaternion(double x, double y, double z, double w) : x(x), y(y), z(z), w(w) {}
+		
+		quaternion operator+(quaternion a) const {
+			return quaternion(x + a.x, y + a.y, z + a.z, w + a.w);
+		}
+		quaternion operator-() const {
+			return quaternion(-x, -y, -z, -w);
+		}
+		quaternion operator-(quaternion a) const {
+			return quaternion(x - a.x, y - a.y, z - a.z, w - a.w);
+		}
+		quaternion operator*(double scale) const {
+			return quaternion(x * scale, y * scale, z * scale, w * scale);
+		}
+		quaternion operator*(quaternion a) const {
+			double A = (y * a.z) - (z * a.y);
+			double B = (z * a.x) - (x * a.z);
+			double C = (x * a.y) - (y * a.x);
+			double D = ((x * a.x) + (y * a.y)) + (z * a.z);
+			return quaternion(x * a.x + a.x * w + A, y * a.w + w * a.y + B, z * a.w + w * a.z + C, w * a.w - D);
+		}
+		bool operator==(quaternion a) const {
+			return x == a.x && y == a.y && z == a.z && w == a.w;
+		}
+		bool operator!=(quaternion a) const {
+			return !(x == a.x && y == a.y && z == a.z && w == a.w);
+		}
 	};
 }
